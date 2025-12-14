e8e5d937ca37c254eaf21d0b048ee53f49fdef4f
mikhaillukin2007-spec <mikhail.lukin2007@gmail.com>
2025-12-15 02:23:11 +0300

    feat: create octal to rwx permissions converter

---
+++ permission_converter.py
@@ -0,0 +1,12 @@
+octal_str = "644"
+
+# Разбиваем на цифры
+d1, d2, d3 = octal_str[0], octal_str[1], octal_str[2]
+
+# Используем таблицу соответствий
+table = "--- --x -w- -wx r-- r-x rw- rwx".split()
+
+# Получаем индексы
+result = table[int(d1)] + table[int(d2)] + table[int(d3)]
+
+print(result)
